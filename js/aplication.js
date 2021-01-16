3bb0a41c7bdc50a754478aa4170820d92571ab2e
Julian <31766151+ymerenich1998@users.noreply.github.com>
2021-01-16 17:35:14 +0200

    end work

--- js/aplication.js
+++ js/aplication.js
@@ -9,3 +9,22 @@ window.onscroll = function() {
     el.style.boxShadow = "none";
   }
 };
+
+document.querySelector('#copytoclipboard').addEventListener('click', function(event) {
+	var copyText = document.getElementById('embedcode').textContent;
+  var x = document.createElement("INPUT");
+  x.setAttribute("type", "text");
+  x.setAttribute("value", copyText);
+  x.setAttribute("id", "trata");
+  document.body.appendChild(x);
+  x.select();
+  x.setSelectionRange(0, 99999);
+	document.execCommand('copy');
+  var tratata = document.getElementById('trata');
+  x.parentNode.removeChild(x);
+
+  var myTooltipEl = document.getElementById('copytoclipboard')
+  var tooltip = new bootstrap.Tooltip(myTooltipEl)
+  tooltip.hide()
+
+});
